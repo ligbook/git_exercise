6027e234a5189fa77fe1ec396e82b59b5eac21a8
ligbook <ligbook@163.com>
2025-11-10 16:58:10 +0800

    server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+console.log("Server is starting...");
+
+try {
+    express = require('express');
+    bodyParser = require('body-parser');
+    mongoose = require('mongoose');
+    cors = require('cors');
+    dotenv = require('dotenv');
+} catch (e) {
+    console.error("Error loading modules:", e);
+    process.exit(1);
+}
